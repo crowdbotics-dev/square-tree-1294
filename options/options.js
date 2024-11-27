a0694f8a441209401c4587433363fddafa241ae8
Crowdbotics <engineering@crowdbotics.com>
2024-11-27 05:47:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-tree-1294",
+  url: "https://square-tree-1294.botics.co",
+  api: "https://square-tree-1294.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
